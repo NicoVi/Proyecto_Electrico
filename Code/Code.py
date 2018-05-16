7884adb14241e52b090427d84b93654b5c6be2f0
palca2093 <palca2093@gmail.com>
2018-05-16 01:02:34 -0600

    Add letters

---
+++ Code/Code.py
@@ -0,0 +1,21 @@
+import numpy
+
+def main():
+    print('Ejecutando Script...')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+main()
---
+++ Code/Timer.py
@@ -0,0 +1,10 @@
+import time
+import Code
+
+def runCode():
+    #Aquí agregas en contenido de tu script o ejecutas el script dentro de un archivo .py.
+    Code.main()
+    time.sleep(10)
+
+while True:
+    runCode()
